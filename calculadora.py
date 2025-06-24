3509acd568edf02a1708034292aac2bd50eb4e05
{vagadev} <112807265+Fogo1910@users.noreply.github.com>
2025-06-23 23:27:11 -0300

    Update calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,34 @@
+print ('calculadora')
+
+print ('escolha as seguintes operações...')
+print ('soma, divisao,multiplicaçao,subtraçao...')
+escolha = input('digite uma operaçao').strip().lower()
+
+if escolha == '+':
+
+    n =int(input('digite o numero'))
+    n1 =int(input('digite outro'))
+    s = n + n1
+    print(f'{n} + {n1} = {s}')
+     
+
+
+if escolha == 'divisao':
+    n =float(input('digite um numero'))
+    n1 =float(input('digite outro'))
+    d = n / n1     
+    print(f'{n} / {n1} = {d}')
+
+
+if escolha == 'multiplicaçao':
+        n =int(input('digite um numero'))
+        n2 =int(input('digite outro'))
+        m = n * n2
+        print(f'{n} * {n2} = {m}')
+
+
+if escolha == 'subtraçao':
+    n =int(input('digite um numero'))
+    n2 =int(input('digite outro'))
+    r = n - n2
+    print(f'{n} - {n2} = {r}')
