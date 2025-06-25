9071aceb9b8ffc3fe80c0ac0dd36b24a7c71cdc4
DannielBack <112807265+DannielBack@users.noreply.github.com>
2025-06-24 23:19:53 -0300

    Update calculapy

--- calculadora.py
+++ calculadora.py
@@ -1,32 +1,35 @@
-print ('calculadora')
+print ('calculapy')
 
-print ('escolha as seguintes operações...')
-print ('[1*]para soma.\n[2*] para divisao.\n[3*] para multiplicaçao.\[4*]para subtraçao...')
-escolha = input('digite uma operaçao').strip().lower()
+print ('escolha as seguintes operações matemáticas :')
+print ('[1*]para somar.\n[2*] para dividir.\n[3*] para multiplicar.\n[4*]para subtrair')
+choose = input('escolha uma operação :').strip()
 
-if escolha == '1':
-
-    n =int(input('digite o numero'))
-    n1 =int(input('digite outro'))
+if choose == '1':
+    print('escolhestes somar')
+    n =int(input('digite um numero :'))
+    n1 =int(input('digite outro '))
     s = n + n1
     print(f'{n} + {n1} = {s}')
 
 
-if escolha == '2':
+if choose == '2':
+    print('escolhestes dividir')
     n =float(input('digite um numero'))
     n1 =float(input('digite outro'))
     d = n / n1     
     print(f'{n} / {n1} = {d}')
 
 
-if escolha == '3':
+if choose == '3':
+        print('escolhestes multiplicar')
         n =int(input('digite um numero'))
         n2 =int(input('digite outro'))
         m = n * n2
         print(f'{n} * {n2} = {m}')
 
 
-if escolha == '4':
+if choose == '4':
+    print('escolhestes subtrair')
     n =int(input('digite um numero'))
     n2 =int(input('digite outro'))
     r = n - n2
