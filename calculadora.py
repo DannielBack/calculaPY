b4798c3f499edb5ad6b53cbd2b4bd807b9e68dc5
DannielBack <112807265+DannielBack@users.noreply.github.com>
2025-06-24 23:07:04 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,10 @@
 print ('calculadora')
 
 print ('escolha as seguintes operações...')
-print ('soma, divisao,multiplicaçao,subtraçao...')
+print ('[1*]para soma.\n[2*] para divisao.\n[3*] para multiplicaçao.\[4*]para subtraçao...')
 escolha = input('digite uma operaçao').strip().lower()
 
-if escolha == 'soma':
+if escolha == '1':
 
     n =int(input('digite o numero'))
     n1 =int(input('digite outro'))
@@ -12,21 +12,21 @@ if escolha == 'soma':
     print(f'{n} + {n1} = {s}')
 
 
-if escolha == 'divisao':
+if escolha == '2':
     n =float(input('digite um numero'))
     n1 =float(input('digite outro'))
     d = n / n1     
     print(f'{n} / {n1} = {d}')
 
 
-if escolha == 'multiplicaçao':
+if escolha == '3':
         n =int(input('digite um numero'))
         n2 =int(input('digite outro'))
         m = n * n2
         print(f'{n} * {n2} = {m}')
 
 
-if escolha == 'subtraçao':
+if escolha == '4':
     n =int(input('digite um numero'))
     n2 =int(input('digite outro'))
     r = n - n2
